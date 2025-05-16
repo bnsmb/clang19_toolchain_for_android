62be646cefaaa458a739d73ad9ead4c31d48e9ea
Bernd <Bernd.Schemmer@gmx.de>
2025-05-16 14:03:54 +0200

    updated OpenSSL to version 3.5

--- sysroot/etc/security/misc/CA.pl
+++ sysroot/etc/security/misc/CA.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/env perl
+#!/data/local/tmp/sysroot/usr/bin/perl
 # Copyright 2000-2021 The OpenSSL Project Authors. All Rights Reserved.
 #
 # Licensed under the Apache License 2.0 (the "License").  You may not use
