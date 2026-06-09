f303fcb7bfc0e0d49566cd8df7a821fb0d6585da
Bernd <Bernd.Schemmer@gmx.de>
2026-06-09 19:44:07 +0200

    added code to print also the API version and NDK version to the C and C++ source files for testing the compiler

--- sysroot/helloworld_in_c++.cpp
+++ sysroot/helloworld_in_c++.cpp
@@ -1,8 +1,9 @@
-#include <iostream>  // include the input-output stream library
+#include <iostream>
+#include <android/api-level.h>
+#include <android/ndk-version.h>
 
 int main() {
-    std::cout << "Hello, World from a C++ program!" << std::endl;  // output the message to the console
-
+    std::cout << "Hello, World from a C++ program!" << std::endl;
 
 #ifdef __clang__
     std::cout << "Compiled with Clang "
@@ -18,9 +19,36 @@ int main() {
     std::cout << "Compiled with an unknown compiler" << std::endl;
 #endif
 
-    std::cout << "Compiler version: " << __VERSION__ << std::endl;
+    std::cout << "Compiler version string: " << __VERSION__ << std::endl;
 
-   return 0;  // indicate successful completion of the program
+    // NDK Version
+#ifdef __NDK_MAJOR__
+    std::cout << "NDK Version: r" << __NDK_MAJOR__;
+    #ifdef __NDK_MINOR__
+        std::cout << "." << __NDK_MINOR__;
+    #endif
+    #ifdef __NDK_BETA__
+        if (__NDK_BETA__ > 0) std::cout << " beta" << __NDK_BETA__;
+    #endif
+    std::cout << std::endl;
+#else
+    std::cout << "NDK Version: unknown" << std::endl;
+#endif
 
+    // Target API Level (kann undefiniert sein!)
+#ifdef __ANDROID_API__
+    std::cout << "Target API Level (compiled for): " << __ANDROID_API__ << std::endl;
+#else
+    std::cout << "Target API Level: not defined (using default/latest)" << std::endl;
+#endif
+
+    // Laufzeit-API Level des Geräts
+    int deviceApiLevel = android_get_device_api_level();
+    if (deviceApiLevel > 0) {
+        std::cout << "Device API Level (runtime): " << deviceApiLevel << std::endl;
+    } else {
+        std::cout << "Device API Level: unknown (error)" << std::endl;
+    }
+
+    return 0;
 }
-
--- sysroot/helloworld_in_c.c
+++ sysroot/helloworld_in_c.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <android/api-level.h>
+#include <android/ndk-version.h>
 
 int main() {
     printf("Hello, World from a C program!\n");
@@ -11,6 +13,36 @@ int main() {
     printf("Compiled with an unknown compiler\n");
 #endif
 
+    printf("Compiler version string: %s\n", __VERSION__);
+
+    // NDK Version
+#ifdef __NDK_MAJOR__
+    printf("NDK Version: r%d", __NDK_MAJOR__);
+    #ifdef __NDK_MINOR__
+        printf(".%d", __NDK_MINOR__);
+    #endif
+    #ifdef __NDK_BETA__
+        if (__NDK_BETA__ > 0) printf(" beta%d", __NDK_BETA__);
+    #endif
+    printf("\n");
+#else
+    printf("NDK Version: unknown\n");
+#endif
+
+    // Target API Level (kann undefiniert sein!)
+#ifdef __ANDROID_API__
+    printf("Target API Level (compiled for): %d\n", __ANDROID_API__);
+#else
+    printf("Target API Level: not defined (using default/latest)\n");
+#endif
+
+    // Laufzeit-API Level des Geräts
+    int deviceApiLevel = android_get_device_api_level();
+    if (deviceApiLevel > 0) {
+        printf("Device API Level (runtime): %d\n", deviceApiLevel);
+    } else {
+        printf("Device API Level: unknown (error)\n");
+    }
+
     return 0;
 }
-
