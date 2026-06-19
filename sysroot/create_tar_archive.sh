eeb98aadc9c5cc2e590c0a6661b405c3ad9ba2ea
Bernd <Bernd.Schemmer@gmx.de>
2026-06-19 11:20:33 +0200

    the tar archive for release tar files is now compressed

--- sysroot/create_tar_archive.sh
+++ sysroot/create_tar_archive.sh
@@ -5,6 +5,9 @@
 #
 # For more details, see the source code below
 #
+# History
+#   15.06.2026 /bs
+#     release tar files are now compressed tar files; all other tar files are still uncompressed
 #
 
 SYSROOT_DIR="${0%/*}"
@@ -34,10 +37,14 @@ TAR_FILE_VERSION="$( echo "${TAR_FILE_HIST_LINE}" | awk '{ print $2 }' )"
 TAR_FILE_TIMESTAMP="$( echo "${TAR_FILE_HIST_LINE}" | awk '{ print $1 }' )"
 
 
+TAR_COMPRESS_OPTION=""
+
 if [ "$1"x = "release"x ] ; then
   POSTFIX="_release_${TAR_FILE_TIMESTAMP}_"
   
   ${SYSROOT_DIR}/clean_env.sh -x
+
+  TAR_COMPRESS_OPTION="-z"
 
 elif [ "$1"x = "develop"x -o "$1"x = "dev"x -o "$1"x = "devel"x ] ; then
   POSTFIX="_dev_${TAR_FILE_TIMESTAMP}_"
@@ -54,6 +61,6 @@ TAR_FILE="${TARGET_DIR}/${TAR_FILE_NAME}" ;
 echo "Creating the tar file \"${TAR_FILE}\" ..."
 
 
-cd ${BASE_DIR} && ${PREFIX} tar -cf ${TAR_FILE} sysroot ; ls -hl ${TAR_FILE}  
+cd ${BASE_DIR} && ${PREFIX} tar ${TAR_COMPRESS_OPTION} -cf ${TAR_FILE} sysroot ; ls -hl ${TAR_FILE}  
 
 
