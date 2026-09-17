f5ea72330588d583fd53e2da53eb545fa31c7c70
Bernd <Bernd.Schemmer@gmx.de>
2026-09-17 18:01:34 +0200

    the source files are now in the dir ./example and the output files are created in the dir ./out

--- sysroot/compile_examples.sh
+++ sysroot/compile_examples.sh
@@ -4,45 +4,64 @@ if [ "${CLANG_SYSROOT}"x = ""x ] ; then
 fi
 
 
+SOURCEDIR="./examples"
+
+OUTDIR="$PWD/out"
+mkdir -p "${OUTDIR}"
+if [ $? -ne 0 ] ; then
+  echo "ERROR: Can not create the directory \"${OUTDIR}\" "
+  exit 7
+fi
+
+if [ ! -d "${SOURCEDIR}" ] ; then
+ echo "ERROR: The directory \"${SOURCEDIR}\" does not exist"
+ exit 9
+fi
+
+
 echo 
 echo "*** Testing the compiler \"clang\" ...."
 echo
 cd ${CLANG_SYSROOT}
-clang ${CFLAGS} ${LDFLAGS} -D__ANDROID_API__=$API -o helloworld_in_c helloworld_in_c.c  && ./helloworld_in_c
+clang ${CFLAGS} ${LDFLAGS} -D__ANDROID_API__=$API -o ${OUTDIR}/helloworld_in_c ${SOURCEDIR}/helloworld_in_c.c  && ${OUTDIR}/helloworld_in_c
 
 
 echo
 echo "*** Testing the compiler \"clang++\" ..."
 echo
 cd ${CLANG_SYSROOT}
-clang++ ${CPPFLAGS} ${LDFLAGS} -D__ANDROID_API__=$API -o helloworld_in_c++ ./helloworld_in_c++.cpp && ./helloworld_in_c++
+clang++ ${CPPFLAGS} ${LDFLAGS} -D__ANDROID_API__=$API -o ${OUTDIR}/helloworld_in_c++ ${SOURCEDIR}/helloworld_in_c++.cpp && ${OUTDIR}/helloworld_in_c++
 
 echo
 echo "*** Testing the assembler from clang ..."
 echo
 cd ${CLANG_SYSROOT}
-clang -nostdlib -static -Wl,--entry=_start -o helloworld_in_assembler helloworld_in_assembler.s && ./helloworld_in_assembler
+clang -nostdlib -static -Wl,--entry=_start -o ${OUTDIR}/helloworld_in_assembler ${SOURCEDIR}/helloworld_in_assembler.s && ${OUTDIR}/helloworld_in_assembler
 
 echo
 echo "*** Testing the assembler from the binutils ..."
 echo
 cd ${CLANG_SYSROOT}
-${CLANG_SYSROOT}/usr/bin/as -o helloworld_in_assembler_for_as.o helloworld_in_assembler_for_as.s && \
-  ${CLANG_SYSROOT}//usr/bin/ld -o helloworld_in_assembler_for_as helloworld_in_assembler_for_as.o && \
-  ./helloworld_in_assembler_for_as
+${CLANG_SYSROOT}/usr/bin/as -o ${OUTDIR}/helloworld_in_assembler_for_as.o ${SOURCEDIR}/helloworld_in_assembler_for_as.s && \
+  ${CLANG_SYSROOT}//usr/bin/ld -o ${OUTDIR}/helloworld_in_assembler_for_as ${OUTDIR}/helloworld_in_assembler_for_as.o && \
+  ${OUTDIR}/helloworld_in_assembler_for_as
 
 if [ -x ${CLANG_SYSROOT}/usr/gcc/bin/gcc-toolchain/gcc ] ; then
   echo
   echo "*** Testing the compiler \"gcc\" ..."
   echo
-  ${CLANG_SYSROOT}/usr/gcc/bin/gcc-toolchain/gcc -o helloworld_in_c_with_gcc helloworld_in_c.c  && ./helloworld_in_c_with_gcc
+  ${CLANG_SYSROOT}/usr/gcc/bin/gcc-toolchain/gcc -o ${OUTDIR}/helloworld_in_c_with_gcc ${SOURCEDIR}/helloworld_in_c.c  && ${OUTDIR}/helloworld_in_c_with_gcc
 fi
 
 if [ -x ${CLANG_SYSROOT}/usr/gcc/bin/gcc-toolchain/g++ ] ; then
   echo
   echo "*** Testing the compiler \"g++\" ..."
   echo
-  ${CLANG_SYSROOT}/usr/gcc/bin/gcc-toolchain/g++   -o helloworld_in_c++_with_g++  ./helloworld_in_c++.cpp && ./helloworld_in_c++_with_g++
+  ${CLANG_SYSROOT}/usr/gcc/bin/gcc-toolchain/g++   -o ${OUTDIR}/helloworld_in_c++_with_g++  ${SOURCEDIR}/helloworld_in_c++.cpp && ${OUTDIR}/helloworld_in_c++_with_g++
 fi
 
+
 echo
+echo "*** Note: The object files and binaries are in the directory \"${OUTDIR}\" "
+echo
+
