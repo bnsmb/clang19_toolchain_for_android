23752c6461fd05dd6594bfd307b74be644c2bbf9
Bernd <Bernd.Schemmer@gmx.de>
2026-09-17 18:01:50 +0200

    the source files are now in the dir ./example and the output files are created in the dir ./out

--- sysroot/test_all_g++_compiler.sh
+++ sysroot/test_all_g++_compiler.sh
@@ -10,7 +10,17 @@ TOOLCHAIN_DIR="${CLANG_SYSROOT}/usr/gcc/bin/gcc-toolchain"
 
 API_LIST=$( ls  ${TOOLCHAIN_DIR}/aarch64-unknown-linux-android*-g++ | sed "s/.*-android//g" | cut -f1 -d "-" | tr "\n" " "  )
 
-if [ ! -r helloworld_in_c++.cpp ] ; then
+SOURCEDIR="./examples"
+
+OUTDIR="$PWD/out"
+mkdir -p "${OUTDIR}"
+if [ $? -ne 0 ] ; then
+  echo "ERROR: Can not create the directory \"${OUTDIR}\" "
+  exit 7
+fi
+
+
+if [ ! -r ${SOURCEDIR}/helloworld_in_c++.cpp ] ; then
   echo "ERROR: The source file \"helloworld_in_c++.cpp\" does not exist in the current directory"
   exit 5
 fi
@@ -19,7 +29,13 @@ for i in ${API_LIST} ;  do
   echo
   echo "*** Testing ${TOOLCHAIN_DIR}/aarch64-unknown-linux-android${i}-g++ ..."
 
-  ${TOOLCHAIN_DIR}/aarch64-unknown-linux-android${i}-g++  -o helloworld_in_c++_with_g++_$i ./helloworld_in_c++.cpp && \
-     file ./helloworld_in_c++_with_g++_$i && \
-     ./helloworld_in_c++_with_g++_$i 
+  ${TOOLCHAIN_DIR}/aarch64-unknown-linux-android${i}-g++  -o ${OUTDIR}/helloworld_in_c++_with_g++_$i ${SOURCEDIR}/helloworld_in_c++.cpp && \
+     file ${OUTDIR}/helloworld_in_c++_with_g++_$i && \
+     ${OUTDIR}/helloworld_in_c++_with_g++_$i 
 done
+
+echo
+echo "*** Note: The object files and binaries are in the directory \"${OUTDIR}\" "
+echo
+
+
--- sysroot/test_all_gcc_compiler.sh
+++ sysroot/test_all_gcc_compiler.sh
@@ -8,7 +8,16 @@ TOOLCHAIN_DIR="${CLANG_SYSROOT}/usr/gcc/bin/gcc-toolchain"
 
 API_LIST=$( ls  ${TOOLCHAIN_DIR}/aarch64-unknown-linux-android*-gcc | sed "s/.*-android//g" | cut -f1 -d "-" | tr "\n" " "  )
 
-if [ ! -r helloworld_in_c.c ] ; then
+SOURCEDIR="./examples"
+
+OUTDIR="$PWD/out"
+mkdir -p "${OUTDIR}" 
+if [ $? -ne 0 ] ; then
+  echo "ERROR: Can not create the directory \"${OUTDIR}\" "
+  exit 7
+fi
+
+if [ ! -r ${SOURCEDIR}/helloworld_in_c.c ] ; then
   echo "ERROR: The source file \"helloworld_in_c.c\" does not exist in the current directory"
   exit 5
 fi
@@ -16,9 +25,13 @@ fi
 for i in ${API_LIST} ; do
   echo 
   echo "Testing ${TOOLCHAIN_DIR}/aarch64-unknown-linux-android${i}-gcc ..."
-  ${TOOLCHAIN_DIR}/aarch64-unknown-linux-android${i}-gcc -o helloworld_in_c_with_gcc_$i helloworld_in_c.c  && \
-     file ./helloworld_in_c_with_gcc_$i  && \
-     ./helloworld_in_c_with_gcc_$i
+  ${TOOLCHAIN_DIR}/aarch64-unknown-linux-android${i}-gcc -o ${OUTDIR}/helloworld_in_c_with_gcc_$i ${SOURCEDIR}/helloworld_in_c.c  && \
+     file ${OUTDIR}/helloworld_in_c_with_gcc_$i  && \
+     ${OUTDIR}/helloworld_in_c_with_gcc_$i
 done
+echo
+
+echo
+echo "*** Note: The object files and binaries are in the directory \"${OUTDIR}\" "
 echo
 
